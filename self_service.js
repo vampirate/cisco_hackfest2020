9e26040c11288489ea2f47ea3d2ec3ee6f4db38e
Yun Yee Ng (AU) <yunyee.ng@global.ntt>
2020-11-21 13:41:13 +1100

    Added tobacco & alcohol use questions

--- self_service.js
+++ self_service.js
@@ -15,3 +15,21 @@ function show_hide_miscarriage_no_question(){
         document.getElementById('miscarriage_no_question').style.display = "none";
     }
 }
+
+function show_hide_tobacco_no_question(){
+    var miscarriage = document.getElementById('tobacco_use').value;
+    if (miscarriage=="Yes") {
+        document.getElementById('tobacco_no_question').style.display = "";
+    } else{
+        document.getElementById('tobacco_no_question').style.display = "none";
+    }
+}
+
+function show_hide_alcohol_no_question(){
+    var miscarriage = document.getElementById('alcohol_use').value;
+    if (miscarriage=="Yes") {
+        document.getElementById('alcohol_no_question').style.display = "";
+    } else{
+        document.getElementById('alcohol_no_question').style.display = "none";
+    }
+}
